c33cb5e33be98de0ad60aef1993ab8510fd4b19c
Carlos Sosa <cjsq24@gmail.com>
2022-07-31 22:11:49 -0400

    change variable

--- 10-change-machine.js
+++ 10-change-machine.js
@@ -23,7 +23,7 @@
 
 const getCoins = (change) => {
   coins = [1, 2, 5, 10, 20, 50];
-  let changeCoins = Array(6).fill(0);
+  const changeCoins = Array(6).fill(0);
   for (let i = coins.length - 1; i >= 0; i--) {
     if (change === 0) break;
     if (coins[i] <= change) {
